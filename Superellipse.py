c59bbfc5ec31aaf6072e7c934b87a68f54ccf7b9
vol <vol@it-kartellet.dk>
2022-08-21 12:07:20 +0200

    Added paramteric equation for the super ellipse

--- Superellipse.py
+++ Superellipse.py
@@ -1,7 +1,7 @@
 #Author-
 #Description-
 
-import adsk.core, adsk.fusion, adsk.cam, traceback
+import adsk.core, adsk.fusion, adsk.cam, traceback, math
 
 def run(context):
     ui = None
@@ -13,3 +13,19 @@ def run(context):
     except:
         if ui:
             ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
+
+# Math
+
+def sgn(x: float) -> float:
+    return 1 if x > 0 else -1
+
+def parametric_superellipse(t, a=1, b=1, n=2.5):
+    """
+    Parametric equation for a superellipse
+    """
+
+    x = a * sgn(math.cos(t)) * abs(math.cos(t)) ** (2 / n)
+    y = b * sgn(math.sin(t)) * abs(math.sin(t)) ** (2 / n)
+
+    return (x, y)
+
